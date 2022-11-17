d590b00f8308d4388ca2114a6731883c707e91ff
inacioalv <51245154+inacioalv@users.noreply.github.com>
2022-11-17 10:03:48 -0300

    manytomany

---
+++ Relacionamento-ManyToMany/src/main/java/br/com/inacio/ManyToMany/RelacionamentoManyToManyApplication.java
@@ -0,0 +1,13 @@
+package br.com.inacio.ManyToMany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacionamentoManyToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelacionamentoManyToManyApplication.class, args);
+	}
+
+}
---
+++ Relacionamento-ManyToMany/src/test/java/br/com/inacio/ManyToMany/RelacionamentoManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inacio.ManyToMany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionamentoManyToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
